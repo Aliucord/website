e8faf3d3d7bb7df97212acc89aa63641e9b47b1a
Lamp <158360149+LampDelivery@users.noreply.github.com>
2026-01-19 12:47:54 -0500

    Update Plugins.tsx

--- client/src/pages/Plugins.tsx
+++ client/src/pages/Plugins.tsx
@@ -33,7 +33,7 @@ export default function Plugins() {
             Browse hundreds of community-made plugins. 
             <span className="block mt-4 p-4 bg-primary/5 border border-primary/20 rounded-2xl text-primary font-medium">
               This page is <strong>view-only</strong>. To get plugins, you must use 
-              <strong>Aliucord</strong> to install using the <strong>#plugins-list</strong> channel or <strong>lumi</strong> bot on our Discord server.
+              <strong>Aliucord</strong> to install with the <strong>#plugins-list</strong> channel or <strong>lumi</strong> bot on our Discord server.
             </span>
           </p>
         </div>
