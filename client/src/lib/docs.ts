f68c1b73aaf91f43d995c88f10f2a629e20d2314
Lamp <158360149+LampDelivery@users.noreply.github.com>
2026-01-19 04:25:13 -0500

    Fix warning

--- client/src/lib/docs.ts
+++ client/src/lib/docs.ts
@@ -893,9 +893,12 @@ Notice from the AliucordRN repository
 We decided to keep all development on the good old 126.21 version of Discord, which is the last version of Discord that doesn't use React Native framework. It may lack of new features that Discord added in the past year, but we should be able to keep existing functionality and to add some new features. [...] Most of our developers just disliked working and using Discord React Native (its performance is really poor and is full of bugs, there are many reasons to just not like it). If you still want to use new Discord with mods, the best choice is Vendetta (made by different team, unaffiliated).
 Source: [GitHub](https://github.com/Aliucord/AliucordRN#-notice-)
 4. If Aliucord ceases to exist, there wouldn't be any active clients using the good old Discord version.
-Bluecord
-Bluecord is also a modded client for the old version of Discord, but the main developer is not a good person, so we don't recommend using it.
-Source: [Google Drive](https://drive.google.com/drive/mobile/folders/1Y2m2lMSpN3GlOcXyceaO88Ljnr8xuNcp)
+
+<blockquote class="warning">
+<strong>Warning: Bluecord</strong><br>
+Bluecord is also a modded client for the old version of Discord, but the main developer is not a good person, so we don't recommend using it.<br>
+Source: <a href="https://drive.google.com/drive/mobile/folders/1Y2m2lMSpN3GlOcXyceaO88Ljnr8xuNcp">Google Drive</a>
+</blockquote>
 5. Features from new Discord versions can be backported to the old one.
 What does backporting mean?
 Backporting is the process of porting a software update that was developed for a relatively current version of a software entity, to an older version of the software.
