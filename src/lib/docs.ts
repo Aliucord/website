e8c3805c39f645a4e6988b774401e38104f9d94f
yutaplug <213663917+yutaplug@users.noreply.github.com>
2026-01-21 15:54:59 +0100

    Update docs.ts

--- src/lib/docs.ts
+++ src/lib/docs.ts
@@ -234,9 +234,11 @@ Alternatively, you can search for another modified Discord client that uses the 
 - Everything would need to be rewritten, wasting time, considering how bad the new Discord version is.
 - Aliucord developers don't like to work with the new one. [Notice from the AliucordRN repository](https://github.com/Aliucord/AliucordRN#-notice-)
 - If Aliucord ceases to exist, there wouldn't be any active clients using the good old Discord version.
+
 <p>warning:
 Bluecord is also a modded client for the old version of Discord, but the main developer is not a good person, so we don't recommend using it.<br>
 Source: <a href="https://drive.google.com/drive/mobile/folders/1Y2m2lMSpN3GlOcXyceaO88Ljnr8xuNcp">Google Drive</a></p>
+
 - Features from new Discord versions can be backported to the old one. [What does backporting mean?](https://en.wikipedia.org/wiki/Backporting)
 - Modded clients for the new Discord version already exist, such as [Kettu](https://github.com/C0C0B01/KettuManager/releases).`
   },
