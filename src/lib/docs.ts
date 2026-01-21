116164ee29ef30dfd6aefbaedab96786a6ba5189
yutaplug <213663917+yutaplug@users.noreply.github.com>
2026-01-21 15:52:31 +0100

    Update docs.ts

--- src/lib/docs.ts
+++ src/lib/docs.ts
@@ -21,9 +21,8 @@ export const GENERAL_DOCS: DocSection[] = [
   {
     title: "Backports",
     content: `**Legend**
-💣: Broken or partially broken
-
-🚧: Still in development or beta
+- 💣: Broken or partially broken
+- 🚧: Still in development or beta
 
 **Note:** To install the plugins from here you need to click the plugin name, it will download the plugin .zip, now just move it to the Aliucord/plugins folder using a file manager & restart Aliucord in case it was open.
 
@@ -231,21 +230,15 @@ Alternatively, you can search for another modified Discord client that uses the 
   },
   {
     title: "Old UI",
-    content: `1. React Native is very slow compared to Kotlin. RN is a framework for building apps using JavaScript, which is not as performant as Kotlin code. Discord's React Native version is known to have performance issues, especially on lower-end devices, because the app has been ported from iOS over to Android.
-    
-2. Everything would need to be rewritten, wasting time, considering how bad the new Discord version is.
-
-3. Aliucord developers don't like to work with the new one. [Notice from the AliucordRN repository](https://github.com/Aliucord/AliucordRN#-notice-)
-
-4. If Aliucord ceases to exist, there wouldn't be any active clients using the good old Discord version.
-
+    content: `- React Native is very slow compared to Kotlin. RN is a framework for building apps using JavaScript, which is not as performant as Kotlin code. Discord's React Native version is known to have performance issues, especially on lower-end devices, because the app has been ported from iOS over to Android.
+- Everything would need to be rewritten, wasting time, considering how bad the new Discord version is.
+- Aliucord developers don't like to work with the new one. [Notice from the AliucordRN repository](https://github.com/Aliucord/AliucordRN#-notice-)
+- If Aliucord ceases to exist, there wouldn't be any active clients using the good old Discord version.
 <p>warning:
 Bluecord is also a modded client for the old version of Discord, but the main developer is not a good person, so we don't recommend using it.<br>
 Source: <a href="https://drive.google.com/drive/mobile/folders/1Y2m2lMSpN3GlOcXyceaO88Ljnr8xuNcp">Google Drive</a></p>
-
-5. Features from new Discord versions can be backported to the old one. [What does backporting mean?](https://en.wikipedia.org/wiki/Backporting)
-
-6. Modded clients for the new Discord version already exist, such as [Kettu](https://github.com/C0C0B01/KettuManager/releases).`
+- Features from new Discord versions can be backported to the old one. [What does backporting mean?](https://en.wikipedia.org/wiki/Backporting)
+- Modded clients for the new Discord version already exist, such as [Kettu](https://github.com/C0C0B01/KettuManager/releases).`
   },
   {
     title: "Sounds",
@@ -256,6 +249,7 @@ Source: <a href="https://drive.google.com/drive/mobile/folders/1Y2m2lMSpN3GlOcXy
   - Once uploaded, click its name
   - Hold View raw and copy the address
   - Paste the link into the plugin
+
 Locally:
   - Install [this file manager](https://play.google.com/store/apps/details?id=me.zhanghai.android.files) & open it (or any file manager that lets you copy file paths)
   - Find the sound file
